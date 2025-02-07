f5d223bd143c2b6d3441cbedbf1c2840e5e32f05
drinklessmilktea <caiyiwen@bupt.edu.cn>
2025-02-07 16:32:57 +0800

    complete lab01

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CS61C
 
 this repo is my answer about labs of CS61C
+
+
--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,22 @@
-#include <stddef.h>
 #include "ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+  /* your code here */
+  node *first = head;
+  if (first == NULL)
     return 0;
+  node *second = head->next;
+  while (second != NULL) {
+    if (second == first)
+      return 1;
+    second = second->next;
+    if (second == NULL) {
+      return 0;
+    } else {
+      second = second->next;
+    }
+    first = first->next;
+  }
+  return 0;
 }
