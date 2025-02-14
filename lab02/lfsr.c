ee9ecb8dc5bebf21a8cb893334c3cf801c40a2b9
drinklessmilktea <caiyiwen@bupt.edu.cn>
2025-02-14 22:45:13 +0800

    lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
-}
+int get_bit(uint16_t x, int n) { return (x >> n) & 1; }
 
+void lfsr_calculate(uint16_t *reg) {
+  /* YOUR CODE HERE */
+  int res =
+      get_bit(*reg, 0) ^ get_bit(*reg, 2) ^ get_bit(*reg, 3) ^ get_bit(*reg, 5);
+  *reg >>= 1;
+  *reg = *reg | (res << 15);
+}
--- lab02/lfsr.h
+++ lab02/lfsr.h
@@ -1,1 +1,6 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 void lfsr_calculate(uint16_t *reg);
